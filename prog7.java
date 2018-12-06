bd63f407900a4ae3f55c91196cb367d778d642e8
MadhabaPatra <42464927+MadhabaPatra@users.noreply.github.com>
2018-12-06 05:55:44 -0800

    init
    method has been improved

--- prog7.java
+++ prog7.java
@@ -1,37 +1,30 @@
 //program 7- the  10001st prime number
 public class prog7
 {
- public static boolean isprime(int n)
- {
-     int i,c=0;
-     boolean flag=false;
-     for(i=1;i<=n;i++)
-     {
-         if (n%i==0)
-         {
-             c++;
-         }
-     }
-        if(c==2)
-        flag=true;
-        return flag;
- }
+ 
  public static void main(String[] args)
  {
-     int c=0,i=1;
-     while(i>0)
+     int i=3,j,n=1;
+ boolean flag;
+ while(n!=10001)
+ {
+   flag=false;
+     for(j=2;j<i;j++)
      {
-         if(isprime(i))
+         if (i%j==0)
          {
-             c++;
-             if(c==10001)
-             {
-                 System.out.println("The prime number at 10001 position="+i);
-                 break;
-                }
-            }
-            i++;
-        }
+             flag=true;
+         break;
+         }
+         }
+     if (flag!=true)
+     {
+         n++;
+     }
+     i++;
+ }
+ System.out.println(i-1);
+
     }
 }
      
