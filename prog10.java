d94df5c811099ba4406e503bfcdb04ee521500fe
MadhabaPatra <42464927+MadhabaPatra@users.noreply.github.com>
2018-12-06 06:17:43 -0800

    init
    method has been improved

--- prog10.java
+++ prog10.java
@@ -1,35 +1,31 @@
 //program 10- the sum of all the prime below 2 million
 public class prog10
 {
- public static boolean isprime(int n)
- {
-     int i,c=0;
-     boolean flag=false;
-     for(i=1;i<=Math.sqrt(n);i++)
-     {
-         if (n%i==0)
-         {
-             c++;
-         }
-     }
-        if(c==1)
-        flag=true;
-        return flag;
- }
+ 
  public static void main(String[] args)
  {
-     int i;long sum=2;
-     for(i=3;i<=2000000;i+=2)
+     long i=3,j,sum=2;
+ 
+ boolean flag;
+ while(i<2000000)
+ {
+     flag=false;
+     for(j=2;j<i;j++)
+     {
+         if (i%j==0)
+         {
+             flag=true;
+         break;
+         }
+         }
+     if (flag!=true)
      {
         
-         
-            if(isprime(i))
-            {
-             sum+=i;
-            }
-        
-    }
-        System.out.println("Sum of prime number="+sum);
+         sum=sum+i;
+     }
+     i++;
+ }
+ System.out.println(sum);
     
 }
 }
