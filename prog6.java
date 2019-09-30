b1c13667a3501cb65d202548196539e11140bfdf
PritamKar <42296778+pikaz143@users.noreply.github.com>
2019-10-01 00:06:42 +0530

    Update prog6.java

--- prog6.java
+++ prog6.java
@@ -3,15 +3,10 @@ public class prog6
 {
  public static void main(String args[])
  {
-     int sum1=0,sum2=0,i,diff,a=1;
-     for(i=1;i<=100;i++)
-     {
-         sum1=sum1+i*i;
-         sum2=sum2+i;
-        }
-        a=Math.pow(sum2,2);
-        diff=a-sum1;
-        System.out.println("result ="+diff);
+     int s1 = 0, s2=0;
+     s1=(100*101*201)/6;
+     s2=(100*101)/2;
+     System.out.println((s2*s2)-s1);
     }
 }
 
