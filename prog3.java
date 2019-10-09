9414576d8eda933a7cc6b5f6f3eb52a38ee1dffd
Subham Sagar Paira <42293958+subhamsagar524@users.noreply.github.com>
2019-10-09 11:47:44 +0530

    Optimised the primality condition.

--- prog3.java
+++ prog3.java
@@ -4,28 +4,16 @@
 {
   public static void main(String args[])
   {
-     int i,max=0;
-      long a=600851475143L;
-      while(a>1)
-      {
-          for(i=2;i<=a;i++)
-          {
-              if(a%i==0)
-              {
-                  if(max<i)
-                  {
-                      max=i;
-                  }
-                  break;
-              }
+     int i;
+     long a=600851475143L;
+     
+     for (i = 2; i <= Math.sqrt(a); i++)
+     {
+          while (a % i == 0) {
+               a /= i;
           }
-          a=a/i;
-      }
-        System.out.println("The largest prime factor="+max);
-    }
-}
-
-        
-                    
-                      
-                     
+     }
+   
+     System.out.println("The largest prime factor="+a);
+  }
+ }
