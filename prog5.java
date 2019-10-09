9f3b541f666f2ad865c97ea9f6c8ea305acba58f
Subham Sagar Paira <42293958+subhamsagar524@users.noreply.github.com>
2019-10-09 12:18:37 +0530

    Optimised
    Used the LCM and HCF method

--- prog5.java
+++ prog5.java
@@ -3,32 +3,25 @@ public class prog5
 {
  public static void main(String args[])
  {
-     int n=1,c=0,i;
-     while(c<20)
-     {
-         c=0;
-         for(i=1;i<=20;i++)
-         {
-             if(n%i==0)
-             {
-                 c++;
-                
-             }
-         }
-          n++;
+     long n=1;
+     long i;
+     for (i = 11; i <= 20; i++) {
+          n = lcm(n,i);
      }
-       System.out.println("The smallest number that can be divisible from 1 to 20 ="+(n-1));
+     System.out.println("The smallest number that can be divisible from 1 to 20 = " + n);
  }
+ 
+ public static long lcm (long x, long y) {
+      return ((x * y) / hcf (x , y));
+              }
+              
+ public static long hcf (long x, long y) {
+      while (x != y) {
+           if (x > y)
+                x -= y;
+           else
+                y -= x;
+      }
+      return x;
+  }
 }
-            
-               
-               
-            
-     
-    
-
-
-            
-           
-            
-    
